ba4fec7b97c86023c88474ad3b69213d08792da2
Daniel O'Connor <darius@dons.net.au>
2022-09-25 15:01:06 +0930

    Add all other known header items.

--- GXCodeWriter.py
+++ GXCodeWriter.py
@@ -2,6 +2,7 @@
 # GXCode is released under the terms of the BSD 2 clause license
 
 import io
+import pdb
 import struct
 import traceback
 
@@ -10,9 +11,13 @@ from cura.Utils.Threading import call_on_qt_thread
 from PyQt6.QtCore import QBuffer, QByteArray
 import UM.Mesh.MeshWriter
 from UM.Logger import Logger
+from UM.Qt.Duration import DurationFormat
 import UM.PluginRegistry
+import UM.Application
 
 gxcode_header_fmt = '<16sIIIIIIIHHHHHHH'
+
+dev = False
 
 class GXCodeWriter(UM.Mesh.MeshWriter.MeshWriter):
     # Need to run on the Qt thread otherwise grabbing snapshots won't work
@@ -24,6 +29,8 @@ class GXCodeWriter(UM.Mesh.MeshWriter.MeshWriter):
         except Exception as e:
             Logger.log('e', 'Unable to write file: %s' % (e))
             Logger.log('e', traceback.format_exc())
+            if dev:
+                pdb.set_trace()
             return False
 
     def dowrite(self, stream):
@@ -42,22 +49,35 @@ class GXCodeWriter(UM.Mesh.MeshWriter.MeshWriter):
         pngofs = bmpofs + bmpsize
         gcodeofs = pngofs + pngsize
 
-        # Other header parameters
+        app = UM.Application.Application.getInstance()
+        exts = app.getExtruderManager().getActiveExtruderStacks()
+        extemp = int(exts[0].getProperty('material_print_temperature', 'value'))
+        if len(exts) > 1:
+            ex2temp = int(exts[1].getProperty('material_print_temperature', 'value'))
+        else:
+            ex2temp = extemp
+        nshells = exts[0].getProperty('wall_line_count', 'value')
+        ptemp = int(exts[0].getProperty('material_bed_temperature', 'value'))
+        pspeed = int(exts[0].getProperty('speed_print', 'value'))
+
+        pinf = app.getPrintInformation()
+        filusage = int(sum(pinf.materialLengths) * 1000) # metres to millimetres
+
+        ptime = int(pinf.currentPrintTime.getDisplayString(DurationFormat.Format.Seconds))
+
+        # Dummy up other header parameters
         unk2 = 0
-        ptime = 500
-        filusage = 500
         unk3 = 0
         unk4 = 0
-        nshells = 2
-        pspeed = 10
-        ptemp = 55
-        extemp = 210
-        ex2temp = 210
         unk5 = 0
+
+        # Break to debugger
+        if dev:
+            pdb.set_trace()
 
         # Assemble header blob
         hdr = struct.pack(gxcode_header_fmt, b'xgcode 1.0\n\x00\x00\x00\x00\x00',
-                          bmpofs, pngofs, gcodeofs, unk2, ptime, filusage, unk3,
+                          bmpofs, pngofs, gcodeofs, ptime, filusage, unk2, unk3,
                           unk4, nshells, pspeed, ptemp, extemp, ex2temp, unk5)
 
         # Write everything out
